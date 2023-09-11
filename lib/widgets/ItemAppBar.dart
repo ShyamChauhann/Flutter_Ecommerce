236f1a4e125bb8f856a8b19a8d8a4d92b8d5487e
Shyam Chauhan <shyamchauhan5702@gmail.com>
2023-09-11 18:00:59 +0530

    11-09-23

--- lib/widgets/ItemAppBar.dart
+++ lib/widgets/ItemAppBar.dart
@@ -1,9 +1,41 @@
 import 'package:flutter/material.dart';
 
-class ItemAppBar extends StatelessWidget{
+class ItemAppBar extends StatelessWidget {
   @override
-  Widget build (BuildContext context)
-  {
-    return Container();
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      padding: EdgeInsets.all(25),
+      child: Row(
+        children: [
+          InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Icon(
+              Icons.arrow_back,
+              size: 30,
+              color: Color(0xff4c53a5),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20),
+            child: Text(
+              "Product",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 23,
+                color: Color(0xff4c53a5),
+              ),
+            ),
+          ),
+          Spacer(),
+          Icon(
+            Icons.favorite,
+            size: 30,
+          ),
+        ],
+      ),
+    );
   }
 }
