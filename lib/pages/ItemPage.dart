f11930b211aee9bdf7265ced140eadd348ec3758
Shyam Chauhan <shyamchauhan5702@gmail.com>
2023-09-14 12:25:56 +0530

    14-09-23

--- lib/pages/ItemPage.dart
+++ lib/pages/ItemPage.dart
@@ -1,18 +1,47 @@
+import 'package:clippy_flutter/arc.dart';
 import 'package:ecommerce_app/widgets/ItemAppBar.dart';
 import 'package:flutter/material.dart';
 
 class ItemPage extends StatelessWidget {
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFEDECF2),
       body: ListView(
         children: [
           ItemAppBar(),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Image.asset(
+              "images/1.png",
+              height: 250,
+            ),
+          ),
+          Arc(
+            edge: Edge.TOP,
+            arcType: ArcType.CONVEY,
+            height: 30,
+            child: Container(
+              width: double.infinity,
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                        top: 50,
+                        bottom: 20,
+                      ),
+                      child: Row,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
-
 }
